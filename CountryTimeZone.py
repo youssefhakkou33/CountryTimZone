c407d40b206a62a21d1eaeb72087866e7b629b27
youssefhakkou33 <youssef.hakkou33@gmail.com>
2023-11-11 20:18:31 -0800

    Added user input commands to find city and country

--- CountryTimeZone.py
+++ CountryTimeZone.py
@@ -126,6 +126,41 @@ def infoCountry():
     print("Test")
 
 
+def get_date_and_time(city, country):
+    # Format the city and country in the URL
+    url = f"https://www.timeanddate.com/worldclock/{country.lower()}/{city.lower()}"
+
+    # Send an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the HTML content of the page using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Locate the element containing the date and time
+        clock_element = soup.find("span", {"id": "ct"})
+
+        # Extract the date and time text
+        date_and_time = clock_element.get_text()
+
+        return date_and_time
+    else:
+        return f"Failed to retrieve the web page for {city}, {country}. Status code: {response.status_code}"
+
+# Get user input for the city and country
+city_input = input("Enter a city: ").strip()
+country_input = input("Enter the country: ").strip()
+
+# Call the function and print the result
+result = get_date_and_time(city_input, country_input)
+
+print(f"Date and Time in {city_input.capitalize()}, {country_input.capitalize()}: {result}")
+
+
+
+
+
 
 
 
